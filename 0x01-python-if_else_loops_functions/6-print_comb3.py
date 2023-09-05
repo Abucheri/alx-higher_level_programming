29d1a44f60efa460ccc4f763e184bf6a1ce8cc9e
O'Brien <abucheriobrien@gmail.com>
2023-09-05 23:25:43 +0000

    unique no. combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for dig1 in range(10):
-    for dig2 in range(dig1 +1, 10):
+    for dig2 in range(dig1 + 1, 10):
         if dig1 == 8 and dig2 == 9:
             print("{:d}{:d}".format(dig1, dig2))
         else:
             print("{:d}{:d}".format(dig1, dig2), end=', ')
-
