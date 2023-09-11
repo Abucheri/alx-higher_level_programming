74a456aedf55e3d23e0c36a7c428bf9d5862656a
O'Brien <abucheriobrien@gmail.com>
2023-09-11 22:36:08 +0000

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,34 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+listint_t *reverse_list(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * reverse_list - reverses a linked list
+ *
+ * @head: double pointer to the head of the linked list
+ *
+ * Return: pointer to the new head of the reversed list
+ */
+
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return (prev);
+}
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  *
  * @head: double pointer to the head of the linked list
  *
- * Return: 1, if palindrome
+ * Return: 1, if palidrome
  * else, 0
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int arr[1024];
-	int i = 0, j;
+	listint_t *slow_ptr = *head, *fast_ptr = *head;
+	listint_t *prev_slow_ptr = NULL;
+	listint_t *second_half = NULL;
+	int is_palindrome = 1;
 
 	if (*head == NULL)
 		return (1);
-	while (current != NULL)
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
 	{
-		arr[i] = current->n;
-		current = current->next;
-		i++;
+		fast_ptr = fast_ptr->next->next;
+		prev_slow_ptr = slow_ptr;
+		slow_ptr = slow_ptr->next;
 	}
-	i--;
-	for (j = 0; j <= i / 2; j++)
+	if (fast_ptr != NULL)
+		slow_ptr = slow_ptr->next;
+	second_half = reverse_list(&slow_ptr);
+	while (second_half != NULL)
 	{
-		if (arr[j] != arr[i - j])
-			return (0);
+		if ((*head)->n != second_half->n)
+		{
+			is_palindrome = 0;
+			break;
+		}
+		*head = (*head)->next;
+		second_half = second_half->next;
 	}
-	return (1);
+	prev_slow_ptr->next = reverse_list(&slow_ptr);
+	return (is_palindrome);
 }
