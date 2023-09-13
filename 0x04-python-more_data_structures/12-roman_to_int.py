d9a4380a40ffd025e099f0e34f9f3d96c720e9c3
O'Brien <abucheriobrien@gmail.com>
2023-09-13 23:44:16 +0000

    rom to int:

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+    romDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    pval = 0
+    for char in roman_string[::-1]:
+        val = romDict.get(char)
+        if not val:
+            return 0
+        if val >= pval:
+            res += val
+        else:
+            res -= val
+        pval = val
+        return res
