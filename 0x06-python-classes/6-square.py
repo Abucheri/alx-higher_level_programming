83d2d6c311574aacad775cd59f2aa299c1c99454
O'Brien <abucheriobrien@gmail.com>
2023-09-26 23:55:18 +0000

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,8 +61,8 @@ class Square:
         Raises:
             TypeError: If value is not a tuple of 2 positive integers.
         """
-        if not isinstance(value, tuple) or len(value) != 2 or \
-                not all(isinstance(val, int) and val >= 0 for val in value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(val, int) and val >= 0 for val in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
