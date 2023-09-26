aff27e9859b5614127872dbdb9b376902132bdd2
O'Brien <abucheriobrien@gmail.com>
2023-09-26 20:00:33 +0000

    private size attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defining a Square class"""
+
+
+class Square:
+    """The Square class"""
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
