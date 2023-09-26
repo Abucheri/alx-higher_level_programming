2dca2765eba65f28390535a2e6163045a4d7e4ca
O'Brien <abucheriobrien@gmail.com>
2023-09-26 19:41:16 +0000

    Square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,11 +3,4 @@
 """Defining a Square class"""
 
 class Square:
-    """This is a Square class.
-    This class is used to represent a square shape.
-    Args:
-        None
-    Return:
-        None
-    """
     pass
