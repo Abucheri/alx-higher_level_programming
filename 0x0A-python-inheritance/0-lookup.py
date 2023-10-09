69b908ba53738c5fd7f46c929d70ddffb3b62c7c
O'Brien <abucheriobrien@gmail.com>
2023-10-09 21:03:53 +0000

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""A definition for displaying object/module info."""
+
+
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object."""
+    return dir(obj)
