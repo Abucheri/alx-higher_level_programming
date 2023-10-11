cb51058b18d676e23c75bf9f240cb918e9179528
O'Brien <abucheriobrien@gmail.com>
2023-10-11 02:01:36 +0000

    Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""This script reads stdin line by line and computes metrics.
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>
+Each 10 lines and after a keyboard interruption (CTRL + C),
+it prints those statistics since the beginning:
+        Total file size: File size: <total size>
+        where <total size> is the sum of all previous lines
+        (see input format above)
+        Number of lines by status code:
+            Possible status codes: 200, 301, 400, 401, 403, 404, 405, and 500
+            If a status code doesn’t appear, it doesn’t print anything for
+            that status code.
+            The status codes are printed in ascending order.
+"""
+import sys
+
+
+metrics = {
+        'total_size': 0,
+        'status_codes': {
+            200: 0,
+            301: 0,
+            400: 0,
+            401: 0,
+            403: 0,
+            404: 0,
+            405: 0,
+            500: 0,
+            }
+        }
+line_count = 0
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        if len(parts) > 2:
+            try:
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
+                metrics['total_size'] += file_size
+                if status_code in metrics['status_codes']:
+                    metrics['status_codes'][status_code] += 1
+                line_count += 1
+                if line_count % 10 == 0:
+                    try:
+                        print(f"File size: {metrics['total_size']}")
+                        for code, count in \
+                                sorted(metrics['status_codes'].items()):
+                            if count > 0:
+                                print(f"{code}: {count}")
+                    except BrokenPipeError:
+                        pass
+            except ValueError:
+                pass
+except KeyboardInterrupt:
+    try:
+        print(f"File size: {metrics['total_size']}")
+        for code, count in sorted(metrics['status_codes'].items()):
+            if count > 0:
+                print(f"{code}: {count}")
+    except BrokenPipeError:
+        pass
