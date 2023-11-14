4a5918d540e3b6a3179ad48d8a8370eca45b8345
O'Brien <abucheriobrien@gmail.com>
2023-11-14 22:10:58 +0000

    Temperatures #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- Import the table dump into the hbtn_0c_0 database
+-- Display the average temperature by city ordered by temperature (descending)
+USE hbtn_0c_0;
+source temperatures.sql
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
