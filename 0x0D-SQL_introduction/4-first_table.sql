687ba5a75f203c6d79645511f8b7bba019ee4188
O'Brien <abucheriobrien@gmail.com>
2023-11-14 19:30:08 +0000

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Script to create the table first_table in the current database
+-- Check if the table already exists; if not, create it
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
