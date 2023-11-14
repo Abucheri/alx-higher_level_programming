551ceb82a05890cacae0523bea36cf4434b2fe36
O'Brien <abucheriobrien@gmail.com>
2023-11-14 20:36:30 +0000

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script to list all records of the table second_table ordered by score
+-- Display records with both score and name, ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
