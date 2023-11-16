5caa79cba358bdbd9abf7799590796eeafe556a7
O'Brien <abucheriobrien@gmail.com>
2023-11-16 02:07:22 +0000

    Not my genre update

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,17 +1,13 @@
 -- This script lists all genres not linked to the show Dexter from the specified MySQL database.
-CREATE TEMPORARY TABLE temp_dexter_genres AS
-SELECT DISTINCT tv_genres.id
-       FROM tv_shows
-            JOIN tv_show_genres
-	    ON tv_shows.id = tv_show_genres.show_id
-
-	    JOIN tv_genres
-	    ON tv_show_genres.genre_id = tv_genres.id
-	WHERE tv_shows.title = 'Dexter';
-
-SELECT tv_genres.name
+SELECT DISTINCT tv_genres.name
   FROM tv_genres
-  WHERE tv_genres.id
-        NOT IN (SELECT id
-		  FROM temp_dexter_genres)
+  WHERE tv_genres.id NOT IN
+        (SELECT tv_genres.id
+	   FROM tv_shows
+	        JOIN tv_show_genres
+		ON tv_shows.id = tv_show_genres.show_id
+
+		JOIN tv_genres
+		ON tv_show_genres.genre_id = tv_genres.id
+	   WHERE tv_shows.title = 'Dexter')
   ORDER BY tv_genres.name ASC;
