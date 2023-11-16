38c076aef876bfc38895785be02b0d67f2c12c08
O'Brien <abucheriobrien@gmail.com>
2023-11-16 00:50:40 +0000

    List shows and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- This script lists all shows and their linked genres from the specified MySQL database.
+-- List all shows and their linked genres using one SELECT statement
+SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS name
+  FROM tv_shows
+       LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+
+       LEFT JOIN tv_genres
+       ON tv_show_genres.genre_id = tv_genres.id
+  ORDER BY tv_shows.title ASC, tv_genres.name ASC;
