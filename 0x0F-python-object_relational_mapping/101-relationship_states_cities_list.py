d567027485b68e0454bf21d91e0e6be1dafc5dd8
O'Brien <abucheriobrien@gmail.com>
2023-12-16 21:45:16 +0000

    List relationship update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,13 +12,22 @@ from relationship_state import Base, State
 from relationship_city import City
 
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+def list_states_cities(username, password, db_name):
+    """
+    Lists all State objects and corresponding City objects in the database.
+    """
+    engine = create_engine("mysql+mysqldb://{0}:{1}@localhost:3306/{2}"
+                           .format(username, password, db_name))
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    query = session.query(State).order_by(State.id).all()
+    for state in query:
+        print("{0}: {1}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print("\t{0}: {1}".format(city.id, city.name))
+    session.close()
+
+
+if __name__ == "__main__":
+    username, password, db_name = argv[1], argv[2], argv[3]
+    list_states_cities(username, password, db_name)
