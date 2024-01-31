a16105a54305cb74326bca2c44e6dbfa39d3eefd
O'Brien <abucheriobrien@gmail.com>
2024-01-31 02:30:12 +0000

    Right order_0 update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,25 +2,22 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const movie = JSON.parse(body);
-    const characters = movie.characters;
-
-    characters.forEach(characterUrl => {
-      request.get(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    theCharacters(characters, 0);
   }
 });
+
+function theCharacters (characters, idx) {
+  request(characters[idx], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        theCharacters(characters, idx + 1);
+      }
+    }
+  });
+}
