31291ad5eb2eba9c8fa4ffc6a0db8edf3ff7e77f
O'Brien <abucheriobrien@gmail.com>
2024-01-31 01:18:17 +0000

    Star wars Wedge Antilles_0 update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,17 +2,13 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const characterId = 18;
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const moviesWithWedge = films.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(moviesWithWedge.length);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+	: count;
+    }, 0));
   }
 });
