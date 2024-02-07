e4a36d65d6cc58a183ea7ba9d19468ee09ece487
O'Brien <abucheriobrien@gmail.com>
2024-02-07 18:40:12 +0300

    updated initial script

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,2 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const header = document.querySelector('header');
-  header.style.color = '#FF0000';
-});
+const header = document.querySelector('header');
+header.style.color = '#FF0000';
